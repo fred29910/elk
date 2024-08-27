12799aaa70102d36b622f8a8ca66f192855e5bb1
fred921110 <vito86077@gmail.com>
2024-08-27 12:01:02 +0800

    测试数据与返回

--- consumer/cmd/consumer/main.go
+++ consumer/cmd/consumer/main.go
@@ -1,6 +1,47 @@
 package main
 
+import (
+	"log"
+
+	"github.com/nats-io/nats.go"
+)
+
+// const ntsUrl = "127.0.0.1:4222"
+
 func main() {
 
-	// TODO: implement
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer nc.Close()
+
+	// Subscribe
+	sub1, err := nc.Subscribe("updates", func(m *nats.Msg) {
+		log.Printf("sub1 Received a message: %s\n", string(m.Data))
+	})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Set limits of 1000 messages or 5MB, whichever comes first
+	sub1.SetPendingLimits(1000, 5*1024*1024)
+
+	// Subscribe
+	sub2, err := nc.Subscribe("updates", func(m *nats.Msg) {
+		log.Printf("sub2 Received a message: %s\n", string(m.Data))
+	})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	// Set no limits for this subscription
+	sub2.SetPendingLimits(-1, -1)
+
+	// Close the connection
+	nc.Close()
+
+	select {}
 }
--- pusher/cmd/pusher/mian.go
+++ pusher/cmd/pusher/mian.go
@@ -1,6 +1,47 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/nats-io/nats.go"
+)
+
+// const ntsUrl = "127.0.0.1:4222"
+
 func main() {
 
-	// TODO: implement
+	// 连接到本地 NATS 服务器
+	nc, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer nc.Close()
+
+	// 要发布的主题
+	subject := "updates"
+
+	// 要发布的数据
+	data := "Hello, NATS!"
+
+	// 发布消息
+	err = nc.Publish(subject, []byte(data))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Printf("Published data '%s' to subject '%s'\n", data, subject)
+
+	// 刷新队列，确保消息已发送
+	nc.Flush()
+
+	// 检查是否有错误
+	if err := nc.LastError(); err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println("Data pushed successfully!")
 }
